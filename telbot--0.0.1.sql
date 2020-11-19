e1b3adf2807647fc26832d7bf940661d14e16793
Никита Архипов <arkhipov@Nikitas-MacBook-Pro.local>
2020-11-19 22:23:43 +0300

    [FIX] wrong reading and upsert parametrs

--- telbot--0.0.1.sql
+++ telbot--0.0.1.sql
@@ -20,38 +20,41 @@ SELECT pg_catalog.pg_extension_config_dump('telbot.conf', '');
 SELECT pg_catalog.pg_extension_config_dump('telbot.sent_messages', '');
 SELECT pg_catalog.pg_extension_config_dump('telbot.sent_messages_id_seq', '');
 
-CREATE FUNCTION telbot.set_param(param varchar(100), val varchar(100)) RETURNS void 
-    AS $$ INSERT INTO telbot.conf VALUES (param, val) ON CONFLICT (key) DO UPDATE SET value = val WHERE telbot.conf.key = param $$ 
-    LANGUAGE SQL;
+CREATE FUNCTION telbot.set_param(param varchar(100), val varchar(100)) RETURNS void AS 
+$$ 
+    INSERT INTO telbot.conf VALUES (param, val) ON CONFLICT (key) DO UPDATE SET value = val WHERE telbot.conf.key = param;
+$$ 
+LANGUAGE SQL;
 
 CREATE FUNCTION telbot.set_chat_id(param varchar(100)) RETURNS void 
-    AS $$ INSERT INTO telbot.conf VALUES ('chat_id', param) $$
+    AS $$ SELECT telbot.set_param('chat_id', param) $$
     LANGUAGE SQL;
 
 CREATE FUNCTION telbot.set_token(param varchar(100)) RETURNS void 
-    AS $$ INSERT INTO telbot.conf VALUES ('token', param) $$
+    AS $$ SELECT telbot.set_param('token', param) $$
     LANGUAGE SQL;
 
 CREATE FUNCTION telbot.send_message(chat_id varchar(50), token text, message text) RETURNS BOOLEAN
-    AS '$libdir/telbot'
+    AS 'MODULE_PATHNAME', 'send_message'
     LANGUAGE C STRICT;
 
-CREATE FUNCTION telbot.send_message_confd(message text) 
-    RETURNS BOOLEAN AS $$
-        DECLARE 
-            passed BOOLEAN;
-            chat_id VARCHAR(100);
-            token VARCHAR(100);
-        BEGIN
-            SELECT value INTO chat_id FROM telbot.conf WHERE key = 'chat_id';
-            SELECT value INTO token FROM telbot.conf WHERE key = 'token';
-            IF chat_id IS NULL OR token IS NULL THEN
-                RAISE EXCEPTION 'Not all parametrs are configured';
-            END IF;
-            SELECT telbot.send_message(chat_id, token, message) INTO passed;
-            IF passed THEN
-                INSERT INTO telbot.sent_messages (datetime, message) VALUES (now(), message);
-            END IF;
-            RETURN passed;
-        END;
-    $$ LANGUAGE plpgsql;
+CREATE FUNCTION telbot.send_message_confd(message text) RETURNS BOOLEAN AS 
+$$
+    DECLARE 
+        passed BOOLEAN;
+        chat_id VARCHAR(100);
+        token VARCHAR(100);
+    BEGIN
+        SELECT value INTO chat_id FROM telbot.conf WHERE key = 'chat_id';
+        SELECT value INTO token FROM telbot.conf WHERE key = 'token';
+        IF chat_id IS NULL OR token IS NULL THEN
+            RAISE EXCEPTION 'Not all parametrs are configured';
+        END IF;
+        SELECT telbot.send_message(chat_id, token, message) INTO passed;
+        IF passed THEN
+            INSERT INTO telbot.sent_messages (datetime, message) VALUES (now(), message);
+        END IF;
+        RETURN passed;
+    END;
+$$ 
+LANGUAGE plpgsql;
--- telbot.c
+++ telbot.c
@@ -12,9 +12,9 @@ Datum send_message(PG_FUNCTION_ARGS) {
     CURL *curl;
     CURLcode res;
 
-    text *chatId = PG_GETARG_TEXT_P(0);
-    text *token = PG_GETARG_TEXT_P(1);
-    text *message = PG_GETARG_TEXT_P(2);
+    char *chatId = TextDatumGetCString(PG_GETARG_DATUM(0));
+    char *token = TextDatumGetCString(PG_GETARG_DATUM(1));
+    char *message = TextDatumGetCString(PG_GETARG_DATUM(2));
 
     curl_global_init(CURL_GLOBAL_ALL);
 
@@ -22,30 +22,31 @@ Datum send_message(PG_FUNCTION_ARGS) {
     if (curl) {
         int parametrAmount = 2;
         int parametrNamesLength = 11;
-        int messageLength = strlen(VARDATA(message));
-        int chatIdLength = strlen(VARDATA(chatId));
-        int tokenLength = strlen(VARDATA(token));
+        int messageLength = strlen(message);
+        int chatIdLength = strlen(chatId);
+        int tokenLength = strlen(token);
 
         char* urlBase = "https://api.telegram.org/bot%s/sendMessage";
         char* url = (char*)malloc((tokenLength + strlen(urlBase) - 2) * sizeof(char));
-        sprintf(url, urlBase, VARDATA(token));
+        sprintf(url, urlBase, token);
+        
         curl_easy_setopt(curl, CURLOPT_URL, url);
 
-        char* escapedMessage = curl_easy_escape(curl, VARDATA(message), messageLength);
+        char* escapedMessage = curl_easy_escape(curl, message, messageLength);
 
         messageLength = strlen(escapedMessage);
         int generalLength = parametrNamesLength + (parametrAmount - 1) + parametrAmount + messageLength + chatIdLength;
 
         char *parametrs = (char *) malloc(generalLength * sizeof(char));
-        sprintf(parametrs, "chat_id=%s&text=%s", VARDATA(chatId), escapedMessage);
+        sprintf(parametrs, "chat_id=%s&text=%s", chatId, escapedMessage);
 
         curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(parametrs));
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, parametrs);
 
         res = curl_easy_perform(curl);
-        if(res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\ntoken: %s\nchat_id: %s\n",
+                    curl_easy_strerror(res), token, chatId);
             PG_RETURN_BOOL(false);
         }
 
